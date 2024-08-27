720145ed839c845292b40d3abf090a9dbd82eb4b
RayJi <kaikai0428@gmail.com>
2024-08-27 09:47:38 +0800

    reduce size

--- application.09445.js
+++ application.09445.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.13db2.json';
+          this.settingsPath = 'src/settings.0d487.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.f9b8e.js
+++ index.f9b8e.js
@@ -1,4 +1,4 @@
-System.register(["./application.0d296.js"], function (_export, _context) {
+System.register(["./application.09445.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
